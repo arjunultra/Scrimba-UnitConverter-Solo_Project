a2426daac1d2a397ad259158d1fc77d6f1275771
Arjunbalan <arjunbalan1994@gmail.com>
2023-05-31 11:27:40 +0530

    lengthEl fixed

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 const convertBtn = document.getElementById("convert-btn");
+const lengthPara = document.getElementById("length-para");
 let inputValue = document.getElementById("input-text");
 convertBtn.addEventListener("click", function () {
   getLength();
 });
 function getLength() {
   // 1 meter = 3.281 feet
-  let oneMeter = 3.281;
-  let myMeter = inputValue.value * oneMeter;
-  console.log(myMeter);
-  let myFoot = inputValue.value / oneMeter;
-  console.log(Number(myFoot.toFixed(2)));
+  const lengthConvertFactor = 3.281;
+  let meterInFeet = inputValue.value * lengthConvertFactor;
+  const feetAnswer = Number(meterInFeet.toFixed(2));
+  let feetInMeter = inputValue.value / lengthConvertFactor;
+  const meterAnswer = Number(feetInMeter.toFixed(2));
+  let baseString = `${inputValue.value} meters = ${feetAnswer} feet | ${inputValue.value} feet = ${meterAnswer} meters`;
+  lengthPara.innerHTML = baseString;
+}
+function getVolume() {
+  // 1 liter = 0.264 gallon
+  const volumeConvertFactor = 0.264;
 }
